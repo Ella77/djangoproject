6f2fcb7a0b28b9700166028e8efeaf48d55f30c6
Ella <ella@mykoon.com>
2018-08-06 17:46:09 +0900

    template using main html

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -17,18 +17,20 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.햣browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
-            '작업아이템 입력'
+            '작업 아이템 입력'
         )
 
         inputbox.send_keys('공작 깃털 사기')
-        inputbox.send_keys(Keys.Enter)
+        inputbox.send_keys(Keys.ENTER)
 
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: 공작 깃털 사기' for row in rows),
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: 공작 깃털 사기' for row in rows),
+                        "신규 작업이 테이블에 표시되지 않는다"
         )
 
         self.fail('Finish the Test!')
