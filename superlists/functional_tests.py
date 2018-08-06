fd9ce81871c67e0b0a4d46e4858e718bc00b3914
Ella <ella@mykoon.com>
2018-08-06 18:42:05 +0900

    reboot django

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -17,7 +17,7 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
-        inputbox = self.햣browser.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             '작업 아이템 입력'
@@ -25,12 +25,17 @@ class NewVisitorTest(unittest.TestCase):
 
         inputbox.send_keys('공작 깃털 사기')
         inputbox.send_keys(Keys.ENTER)
+        #
+        # import time
+        # time.sleep(10)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
             any(row.text == '1: 공작 깃털 사기' for row in rows),
-                        "신규 작업이 테이블에 표시되지 않는다"
+                        "신규 작업이 테이블에 표시되지 않는다" -- 해당 텍스트 :\n%s" % (
+                            table.text,
+                                    
         )
 
         self.fail('Finish the Test!')
