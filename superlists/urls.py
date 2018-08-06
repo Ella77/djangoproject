2c7c86ca4a49fe3e378480b42a473e40882c05c1
Ella <ella@mykoon.com>
2018-08-06 16:20:23 +0900

    returning html view

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,7 +1,6 @@
-"""superlists URL Configuration
-
+"""urluni URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,9 +12,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.conf.urls import url
+# from django.contrib import admin
 from django.urls import path
 
+from lists import views
+
+
+# from .lists import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home_page, name='home'),
 ]
