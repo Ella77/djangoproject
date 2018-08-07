490d41b67350ed5d8c00192409d5d24dee9b5e2a
Ella <ella@mykoon.com>
2018-08-07 15:16:27 +0900

    model and view using regex

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -7,6 +7,7 @@ import sys
 from contextlib import contextmanager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.expected_conditions import staleness_of
+import time
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -47,6 +48,14 @@ class NewVisitorTest(LiveServerTestCase):
                 break
             except StaleElementReferenceException:
                 continue
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+    #목록 저장하고 있는지 궁금
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get(self.live_server_url)
         self.assertIn('To-Do', self.browser.title)
@@ -58,47 +67,66 @@ class NewVisitorTest(LiveServerTestCase):
             inputbox.get_attribute('placeholder'),
             '작업 아이템 입력'
         )
+        inputbox.send_keys('공작 깃털 사기')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        with self.wait_for_page_load(timeout=10):
+            self.check_for_row_in_list_table('1: 공작 깃털 사기')
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('깃털 모아서 날기')
         inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        with self.wait_for_page_load(timeout=10):
+            self.check_for_row_in_list_table('1: 공작 깃털 사기')
+            self.check_for_row_in_list_table('2: 깃털 모아서 날기')
 
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('공작 깃털 사기', page_text)
+        self.assertNotIn('깃털 모아서 날기', page_text)
 
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('우유 사기')
+        inputbox.send_keys(Keys.ENTER)
 
-        def check_for_first_item():
-            def check_for_row_in_list_table(self, row_text):
-                table = self.browser.find_element_by_id('id_list_table')
-                rows = table.find_elements_by_tag_name('tr')
-                self.assertIn(row_text, [row.text for row in rows])
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
 
-            with self.wait_for_page_load(timeout=10):
-                self.check_for_row_in_list_table('1: 공작 깃털 사기')
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('공작 깃털 사기', page_text)
+        self.assertIn('우유 사기', page_text)
+        # def check_for_first_item():
+        #         # def check_for_second_item():
+        #         #     def check_for_row_in_list_table(self, row_text):
+        #         #         table = self.browser.find_element_by_id('id_list_table')
+        #         #         rows = table.find_elements_by_tag_name('tr')
+        #         #         self.assertIn(row_text, [row.text for row in rows])
+        #         #
+        #         #     with self.wait_for_page_load(timeout=10):
+        #         #         self.check_for_row_in_list_table('2: 깃털 모아서 날기')
+        #         #
+        #         # def insert_second_item_to_inputbox():
+        #         #     inputbox2 = self.browser.find_element_by_id('id_new_item')
+        #         #     self.assertEqual(
+        #         #         inputbox2.get_attribute('placeholder'),
+        #         #         '작업 아이템 입력'
+        #         #     )
+        #         #     inputbox2.send_keys('깃털 모아서 날기')
+        #         #     inputbox2.send_keys(Keys.ENTER)
 
-
-
-        def check_for_second_item():
-            def check_for_row_in_list_table(self, row_text):
-                table = self.browser.find_element_by_id('id_list_table')
-                rows = table.find_elements_by_tag_name('tr')
-                self.assertIn(row_text, [row.text for row in rows])
-
-            with self.wait_for_page_load(timeout=10):
-                self.check_for_row_in_list_table('2: 깃털 모아서 날기')
-
-        def insert_second_item_to_inputbox():
-            inputbox2 = self.browser.find_element_by_id('id_new_item')
-            self.assertEqual(
-                inputbox2.get_attribute('placeholder'),
-                '작업 아이템 입력'
-            )
-            inputbox2.send_keys('깃털 모아서 날기')
-            inputbox2.send_keys(Keys.ENTER)
-
-
-        self.stale_aware_for_action(check_for_first_item)
-
-        self.stale_aware_for_action(insert_second_item_to_inputbox)
-
-        self.stale_aware_for_action(check_for_second_item)
-        self.stale_aware_for_action(check_for_first_item)
+        #
+        # self.stale_aware_for_action(check_for_first_item)
+        #
+        # self.stale_aware_for_action(insert_second_item_to_inputbox)
+        #
+        # self.stale_aware_for_action(check_for_second_item)
+        # self.stale_aware_for_action(check_for_first_item)
         self.fail('Finish the Test!')
 
 
