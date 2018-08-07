85ff8ba75b2bd5a55acd7b09a8876d751a553668
Ella <ella@mykoon.com>
2018-08-07 16:25:36 +0900

    url matching

--- lists/tests.py
+++ lists/tests.py
@@ -9,6 +9,8 @@ from lists.views import home_page
 from lists.models import Item
 
 # Create your tests here.
+
+
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
         first_item = Item()
@@ -27,10 +29,10 @@ class ItemModelTest(TestCase):
 
 
 class HomePageTest(TestCase):
-    def test_home_page_only_saves_items_when_necessary(self):
-        request = HttpRequest()
-        home_page(request)
-        self.assertEqual(Item.objects.count(), 0)
+    # def test_home_page_only_saves_items_when_necessary(self):
+    #     request = HttpRequest()
+    #     home_page(request)
+    #     self.assertEqual(Item.objects.count(), 0)
 
     def remove_csrf(self, origin):
         csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
@@ -58,13 +60,32 @@ class HomePageTest(TestCase):
         # self.assertIn(b'<title>To-Do lists</title>',response.content)
         # self.assertTrue(response.content.strip().endswith(b'</html>'))
 
+
+class ListViewTest(TestCase):
+    def test_uses_list_template(self):
+        response= self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+    #
+    # def test_displays_all_items(self):
+    #     Item.objects.create(text= 'itemey 1')
+    #     Item.objects.create(text= 'itemey 2')
+    #
+    #     response = self.client.get('/lists/the-only-list-in-the-world/')
+    #
+    #     self.assertContains(response, 'itemey 1')
+    #     self.assertContains(response, 'itemey 2')
+
+
+class NewListTest(TestCase):
     def test_home_page_can_save_a_POST_request(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = '신규 작업 아이템'
-
-        response = home_page(request)
-
+        # request = HttpRequest()
+        # request.method = 'POST'
+        # request.POST['item_text'] = '신규 작업 아이템'
+        #
+        # response = home_page(request)
+        self.client.post('/lists/new',
+                         data={'item_text': '신규 작업 아이템'}
+                         )
         #self.assertIn('신규 작업 아이템', response.content.decode())
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
@@ -94,12 +115,19 @@ class HomePageTest(TestCase):
         # self.assertEqual(response_decode, expected_html)
 
     def test_home_page_redirects_after_POST(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = '신규 작업 아이템'
-        response = home_page(request)
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'],'/lists/the-only-list-in-the-world/')
+        # request = HttpRequest()
+        # request.method = 'POST'
+        # request.POST['item_text'] = '신규 작업 아이템'
+        # response = home_page(request)
+        response= self.client.post(
+            '/lists/new',
+            data={'item_text': '신규 작업 아이템'}
+        )
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+        #
+        # self.assertEqual(response.status_code, 302)
+        # self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
+
 
     # def test_home_page_displays_all_list_itmes(self):
     #     Item.objects.create(text='itemey 1')
@@ -110,18 +138,3 @@ class HomePageTest(TestCase):
     #
     #     self.assertIn('itemey 1', response.content.decode())
     #     self.assertIn('itemey 2', response.content.decode())
-
-
-class ListViewTest(TestCase):
-    def test_uses_list_template(self):
-        response= self.client.get('/lists/the-only-list-in-the-world/')
-        self.assertTemplateUsed(response, 'list.html')
-
-    def test_displays_all_items(self):
-        Item.objects.create(text= 'itemey 1')
-        Item.objects.create(text= 'itemey 2')
-
-        response = self.client.get('/lists/the-only-list-in-the-world/')
-
-        self.assertContains(response, 'itemey 1')
-        self.assertContains(response, 'itemey 2')
--- lists/views.py
+++ lists/views.py
@@ -2,16 +2,20 @@ from django.http import HttpResponse
 from django.shortcuts import redirect,render
 from lists.models import Item
 # Create your views here.
+
+
 def home_page(request):
 
     # return HttpResponse('<html><title>To-Do lists</title></html>')
     # if request.method == 'POST':
     #     return HttpResponse(request.POST['item_text'])
     # return render(request,'home.html')
-        if request.method == 'POST':
-            new_item_text = request.POST['item_text']
-            Item.objects.create(text=new_item_text)
-            return redirect('/lists/the-only-list-in-the-world/')
+
+    #     if request.method == 'POST':
+    #         new_item_text = request.POST['item_text']
+    #         Item.objects.create(text=new_item_text)
+    #         return redirect('/lists/the-only-list-in-the-world/')
+
         #create not requiring saving
     # else :
     #     new_item_text= ''
@@ -23,6 +27,12 @@ def home_page(request):
     #     'new_item_text' : new_item_text
     # })\
 
+
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
