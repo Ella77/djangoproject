1342bc9232df3388fe4bfc07fe4572d16417d8c2
Ella <ella@mykoon.com>
2018-08-07 10:12:01 +0900

    add second item list

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -26,35 +26,37 @@ class NewVisitorTest(unittest.TestCase):
 
 
         def insert_second_item_to_inputbox():
-         inputbox = self.browser.find_element_by_id('id_new_item')
+            inputbox = self.browser.find_element_by_id('id_new_item')
             self.assertEqual(
             inputbox.get_attribute('placeholder'),
             '작업 아이템 입력'
-        )
+            )
 
-            inputbox.send_keys('공작 깃털 사기')
+            inputbox.send_keys('깃털 모아서 날기')
             inputbox.send_keys(Keys.ENTER)
-            self.stale_aware_for_action(insert_second_item_to_inputbox)
+
+            table = self.browser.find_element_by_id('id_list_table')
+            rows = table.find_elements_by_tag_name('tr')
+            self.assertIn('1: 공작 깃털 사기', [row.text for row in rows])
+            self.assertIn('2: 깃털 모아서 날기',[row.text for row in rows])
+            self.fail('Finish the Test!')
+        self.stale_aware_for_action(insert_second_item_to_inputbox)
 
         #
        # import time
         #time.sleep(6)
 
-            table = self.browser.find_element_by_id('id_list_table')
-            rows = table.find_elements_by_tag_name('tr')
         #self.assertTrue(
             #any(row.text == '1: 공작 깃털 사기' for row in rows),
            #             "신규 작업이 테이블에 표시되지 않는다 -- 해당 텍스트 :\n%s" % (
           #                  table.text,
          #   )
         #)
-            self.assertIn('1: 공작 깃털 사기', [row.text for row in rows])
 
-            self.fail('Finish the Test!')
         def check_for_first_item():
             self.check_for_row_in_list_table('1: 공작 깃털 사기')
         def check_for_second_item():
-            self.check_for_row_in_list_table('2: 깃털 모아서 날기')\
+            self.check_for_row_in_list_table('2: 깃털 모아서 날기')
         self.stale_aware_for_action(check_for_first_item)
         self.stale_aware_for_action(check_for_second_item)
     # if __name__ == '__main__':
