640fcbe982389e37874097869b4501e9f0b43ec3
Ella <ella@mykoon.com>
2018-08-08 17:04:44 +0900

    ft hacking for staging tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -12,6 +12,28 @@ import time
 
 class NewVisitorTest(StaticLiveServerTestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
+
+    def stale_aware_for_action(self, action):
+        while (True):
+            try:
+                action()
+                break
+            except StaleElementReferenceException:
+                continue
+
     def setUp(self):
         self.browser = webdriver.Firefox(executable_path='/Users/soojin/Downloads/geckodriver')
         # self.browser.implicitly_wait(10)
@@ -23,43 +45,10 @@ class NewVisitorTest(StaticLiveServerTestCase):
             staleness_of(old_page)
         )
 
-    def test_layout_and_styling(self):
-        self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768)
+    def tearDown(self):
+        self.browser.quit()
 
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('testing\n')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=10
-        )
 
-    # @classmethod
-    # def setUpClass(cls):
-    #     for arg in sys.argv :
-    #         if 'liveserver' in arg:
-    #             cls.server_url = 'http://' + arg.split('=')[1]
-    #             cls.live_server_url = ''
-    #             return
-    #     super().setUpClass()
-    #     cls.server_url = cls.live_server_url
-    #
-    # def tearDown(self):
-    #     self.browser.quit()
-    #
-    # @classmethod
-    # def tearDownClass(cls):
-    #     if cls.server_url == cls.live_server_url:
-    #         super().tearDownClass()
-
-    def stale_aware_for_action(self, action):
-        while(True):
-            try:
-                action()
-                break
-            except StaleElementReferenceException:
-                continue
 
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
@@ -70,7 +59,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
@@ -98,7 +87,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         self.browser.quit()
         self.browser = webdriver.Firefox(executable_path='/Users/soojin/Downloads/geckodriver')
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('공작 깃털 사기', page_text)
         self.assertNotIn('깃털 모아서 날기', page_text)
@@ -116,6 +105,18 @@ class NewVisitorTest(StaticLiveServerTestCase):
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('공작 깃털 사기', page_text)
         self.assertIn('우유 사기', page_text)
+
+    def test_layout_and_styling(self):
+            self.browser.get(self.live_server_url)
+            self.browser.set_window_size(1024, 768)
+
+            inputbox = self.browser.find_element_by_id('id_new_item')
+            inputbox.send_keys('testing\n')
+            self.assertAlmostEqual(
+                inputbox.location['x'] + inputbox.size['width'] / 2,
+                512,
+                delta=10
+            )
         # def check_for_first_item():
         #         # def check_for_second_item():
         #         #     def check_for_row_in_list_table(self, row_text):
