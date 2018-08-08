24a664f5da5d85ce535cbba3c87a110fbb7b63b0
Ella <ella@mykoon.com>
2018-08-08 15:07:33 +0900

    STATIC_ROOT configuration and admin deactivate

--- superlists/settings.py
+++ superlists/settings.py
@@ -55,7 +55,8 @@ ROOT_URLCONF = 'superlists.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
+# os.path.join(BASE_DIR, 'templates')
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
+
